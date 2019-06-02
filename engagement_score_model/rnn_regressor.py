Fixed the loss function, added engagement csv calculation, properly divide train/val/test

engagement_score_model/rnn_regressor.py
@@ -1,5 +1,6 @@
 import torch
 import random
+import pandas as pd
 from torchtext import data
 import torch.nn as nn
 import torch.optim as optim
@@ -21,39 +22,37 @@ OUTPUT_DIM = 1
 N_LAYERS = 2
 BIDIRECTIONAL = True
 DROPOUT = 0.5
-N_EPOCHS = 50
+N_EPOCHS = 30
 best_valid_loss = float('inf')
 tPath = '../twitter/data/'
-trainFile = 'harold.csv'
-testFile = 'harold.csv'
-valFile = 'harold.csv'
-tweetFile = '../twitter/data/test.csv'
-userFile = '../clustering/clusters.csv'
-oFile = './engagement.csv'
+trainFile = './train.csv'
+testFile = './test.csv'
+valFile = './val.csv'
+
+df = pd.read_csv(valFile)
+usrGrpCnt = len(df.columns) - 1
+sentCategoryCnt = len(df[df.columns[-1]].unique())
+labelName = 'group1'
 
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 TEXT = data.Field(tokenize = 'spacy', include_lengths = True, lower=True)
-LABEL = data.LabelField(dtype = torch.float)
+LABEL = data.LabelField(dtype = torch.long)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-tarUsrGrp = 0
-engagementDf = util.construct_engagement_score_ds(tweetFile, userFile)
-engagementDf = util.convert_to_categorical( [-999999, -13, -6, 999999], 
-        tarUsrGrp, engagementDf )
-engagementDf.to_csv(oFile, index=False)
-
-csvFields = [   ('text', TEXT), #clean_text
-                ('label', LABEL),
-                ('grp1', None),
-            ]
+csvFields = [   ('text', TEXT) ]
+labelFields = ['group0']
+for userGrp in range( usrGrpCnt ):
+    label = 'group%s' % userGrp
+    csvFields.append( ( label, LABEL ) )
+#    labelFields.append( label )
 
 train_data, valid_data, test_data = data.TabularDataset.splits(
                 path='.', 
-                train=oFile,
-                validation=oFile, 
-                test=oFile, 
+                train=trainFile,
+                validation=valFile, 
+                test=testFile, 
                 format='csv',
                 fields=csvFields,
                 skip_header=True,
@@ -72,10 +71,14 @@ train_iterator, valid_iterator, test_iterator = data.Iterator.splits(
     sort_within_batch = True,
     device = device)
 
+#train_dl = util.BatchWrapper(train_iterator, 'text', labelFields )
+#valid_dl = util.BatchWrapper(valid_iterator, 'text', labelFields )
+#test_dl = util.BatchWrapper(test_iterator, 'text', labelFields)
+
 INPUT_DIM = len(TEXT.vocab)
 PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
 
-model = LTSM(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, OUTPUT_DIM, 
+model = LTSM(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, 1*sentCategoryCnt, 
             N_LAYERS, BIDIRECTIONAL, DROPOUT, PAD_IDX)
 
 pretrained_embeddings = TEXT.vocab.vectors
@@ -84,7 +87,7 @@ UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
 model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
 model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
 optimizer = optim.Adam(model.parameters())
-criterion = nn.MSELoss()
+criterion = nn.CrossEntropyLoss()
 
 model = model.to(device)
 criterion = criterion.to(device)
@@ -98,9 +101,9 @@ for epoch in range(N_EPOCHS):
     start_time = time.time()
     
     train_loss, train_acc = util.train(model, train_iterator, 
-            optimizer, criterion)
+            optimizer, criterion, labelName)
     valid_loss, valid_acc = util.evaluate(model, valid_iterator,
-            criterion)
+            criterion, labelName)
     
     end_time = time.time()
     epoch_mins, epoch_secs = util.epoch_time(start_time, end_time)
@@ -114,8 +117,8 @@ for epoch in range(N_EPOCHS):
     print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
 
 model.load_state_dict(torch.load('lstm_model.pt'))
-test_loss, test_acc = util.evaluate(model, test_iterator, criterion)
+test_loss, test_acc = util.evaluate(model, test_iterator, criterion, labelName)
 print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
 
-util.predict_engagement(model, "This film is terrible", TEXT, device)
-util.predict_engagement(model, "This film is great", TEXT, device)
+util.predict_engagement(model, 'Climate change is terrible', TEXT, device)
+util.predict_engagement(model, 'We need to act now to fix climate change', TEXT, device)

engagement_score_model/util.py
@@ -8,17 +8,17 @@ import pdb
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-def binary_accuracy(preds, y):
+def get_accuracy(preds, y):
     """
     Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
     """
     #round predictions to the closest integer
-    rounded_preds = torch.round(torch.sigmoid(preds))
+    rounded_preds = torch.argmax(torch.softmax(preds,dim=1),dim=1)
     correct = (rounded_preds == y).float() #convert into float for division 
     acc = correct.sum() / len(correct)
     return acc
 
-def train(model, iterator, optimizer, criterion):
+def train(model, iterator, optimizer, criterion, labelName):
     epoch_loss = 0
     epoch_acc = 0
     
@@ -26,9 +26,10 @@ def train(model, iterator, optimizer, criterion):
     for batch in iterator:
         optimizer.zero_grad()
         text, text_lengths = batch.text
+        labels = getattr( batch, labelName )
         predictions = model(text, text_lengths).squeeze(1)
-        loss = criterion(predictions, batch.label)
-        acc = binary_accuracy(predictions, batch.label)
+        loss = criterion(predictions, labels)
+        acc = get_accuracy(predictions, labels)
         loss.backward()
         optimizer.step()
         
@@ -37,7 +38,7 @@ def train(model, iterator, optimizer, criterion):
         
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-def evaluate(model, iterator, criterion):
+def evaluate(model, iterator, criterion, labelName):
     epoch_loss = 0
     epoch_acc = 0
     
@@ -45,9 +46,10 @@ def evaluate(model, iterator, criterion):
     with torch.no_grad():
         for batch in iterator:
             text, text_lengths = batch.text
+            labels = getattr( batch, labelName )
             predictions = model(text, text_lengths).squeeze(1)
-            loss = criterion(predictions, batch.label)
-            acc = binary_accuracy(predictions, batch.label)
+            loss = criterion(predictions, labels)
+            acc = get_accuracy(predictions, labels)
 
             epoch_loss += loss.item()
             epoch_acc += acc.item()
@@ -69,66 +71,25 @@ def predict_engagement(model, sentence, TEXT, device):
     tensor = torch.LongTensor(indexed).to(device)
     tensor = tensor.unsqueeze(1)
     length_tensor = torch.LongTensor(length)
-    prediction = torch.sigmoid(model(tensor, length_tensor))
-    return prediction.item()
+    preds = model(tensor, length_tensor)
+    preds = torch.argmax(torch.softmax(preds,dim=0))
+    print( preds )
 
-###################################
-#This function constructs the engagement score dataset
-#tweetFile is the raw csv file from step 1
-#userFile is the user group assignment from step 2
-##################################
-def construct_engagement_score_ds( tweetFile, userFile ):
-    assert os.path.isfile(tweetFile), 'Invalid Tweet file'
-    assert os.path.isfile(userFile), 'Invalid userFile'
+class BatchWrapper:
+    def __init__(self, dl, x_var, y_vars):
+        self.dl, self.x_var, self.y_vars = dl, x_var, y_vars # we pass in the list of attributes for x and y
 
-    userNameCol = 'screen_name'
-    parentIdCol = 'parent_id'
-    usrGrpCol = 'group'
-    idCol = 'id'
-    egmtCol = 'engagement'
+    def __iter__(self):
+        for batch in self.dl:
+            x = getattr(batch, self.x_var) # we assume only one input in this wrapper
 
-    tweetDf = pd.read_csv( tweetFile )
-    userDf = pd.read_csv( userFile )
+        if self.y_vars is not None: # we will concatenate y into a single tensor
+            y = torch.cat([getattr(batch, feat).unsqueeze(1) for feat in self.y_vars], dim=1).long()
+        else:
+            y = torch.zeros((1))
 
-    #Add user group assignment to tweet Df
-    tweetDf = pd.merge( tweetDf, userDf, how='inner', on=userNameCol )
-    
-    engagementDf = []
-    for pId in tweetDf[parentIdCol].dropna().unique().astype(
-            tweetDf[idCol].dtype):
-        curTweetEntry = []
-        tweet = tweetDf[tweetDf[idCol] == pId]
-        if tweet.shape[0] == 0:
-            print( 'Invalid parent ID %s, continue', pId )
-            continue
-        parentFollower = tweet['follower_count'].values[0]+1
-        curTweetEntry.append( tweet['clean_text'].values[0] ) 
-        replyDf = tweetDf[tweetDf[parentIdCol]==pId]
-        #Shifting to polarity to [0 to 2]
-        replyDf.loc[:,egmtCol] = np.log((( replyDf['favorite_count'] +
-                replyDf['retweet_count'] + 1) * ( replyDf['polarity'] + 1 ) / 
-                parentFollower ).values[0] + 1E-15)
+        yield (x, y)
 
-        for grp in sorted(userDf[usrGrpCol].unique()):
-            if grp in replyDf[usrGrpCol].values:
-                curTweetEntry.append( replyDf[replyDf[usrGrpCol]==grp]
-                        [egmtCol].mean() )
-            else:
-                curTweetEntry.append( -15 )
-        engagementDf.append( curTweetEntry )
-
-    engagementDf = pd.DataFrame( engagementDf )
-    engagementDf.columns = ['text'] + sorted(userDf[usrGrpCol].unique()) 
-    return engagementDf
-
-def convert_to_categorical( thresholds, tarCol, df ):
-    catDf = df.copy()
-    for i in range(1,len(thresholds)):
-        idx = (df[tarCol] < thresholds[i]) & (
-                df[tarCol] >= thresholds[i-1])
-        catDf.loc[idx,tarCol] = i-1
-    return catDf
-
-
-
+    def __len__(self):
+        return len(self.dl)
 
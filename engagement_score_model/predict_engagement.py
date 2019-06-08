820d80f6c4693df35ada2502ff1d18bd0a68c399
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-08 18:58:20 +0000

    changed num tweets

--- engagement_score_model/predict_engagement.py
+++ engagement_score_model/predict_engagement.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+# In[1]:
+
+
+# load the pytorch model
 import torch
 import random
 import pandas as pd
@@ -15,34 +20,35 @@ from nltk.corpus import stopwords
 from nltk.corpus import wordnet 
 from collections import defaultdict, namedtuple
 
-######################################################
-#Hyperparameters and config variables
-######################################################
+
+# In[2]:
+
+
 SEED = 1234
 MAX_VOCAB_SIZE = 25_000
-BATCH_SIZE = 64 * 4
-EMBEDDING_DIM = 100
-HIDDEN_DIM = 256
-OUTPUT_DIM = 1
+BATCH_SIZE = 64 * 32
+EMBEDDING_DIM = 50
+HIDDEN_DIM = int(256 / 4)
 N_LAYERS = 2
 BIDIRECTIONAL = True
 DROPOUT = 0.5
-N_EPOCHS = 30
+N_EPOCHS = 300
 best_valid_loss = float('inf')
 tPath = '../twitter/data/'
-trainFile = './test.csv'
+trainFile = './train.csv'
 testFile = './test.csv'
 valFile = './val.csv'
 
 df = pd.read_csv(valFile)
 usrGrpCnt = len(df.columns) - 1
 sentCategoryCnt = len(df[df.columns[-1]].unique())
+output_dim = 1
 
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 TEXT = data.Field(tokenize = 'spacy', include_lengths = True, lower=True)
-LABEL = data.LabelField(dtype = torch.long)
+LABEL = data.LabelField(dtype = torch.float)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 csvFields = [   ('text', TEXT) ]
@@ -62,27 +68,39 @@ train_data, valid_data, test_data = data.TabularDataset.splits(
 
 TEXT.build_vocab(train_data, 
                  max_size = MAX_VOCAB_SIZE, 
-                 vectors = "glove.6B.100d", 
+                 vectors = "glove.twitter.27B.50d", 
                  unk_init = torch.Tensor.normal_)
 
 INPUT_DIM = 25002
 PAD_IDX = 1
-modelGrp0 = LTSM(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, 1*sentCategoryCnt, 
+modelGrp0 = LTSM(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, output_dim, 
             N_LAYERS, BIDIRECTIONAL, DROPOUT, PAD_IDX)
-modelGrp1 = LTSM(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, 1*sentCategoryCnt, 
+modelGrp1 = LTSM(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, output_dim, 
             N_LAYERS, BIDIRECTIONAL, DROPOUT, PAD_IDX)
 
 model_group_zero = modelGrp0.to(device)
 model_group_one = modelGrp1.to(device)
 
+
+# In[3]:
+
+
 model_group_zero.load_state_dict(torch.load('lstm_model_group0.pt'))
 model_group_one.load_state_dict(torch.load('lstm_model_group1.pt'))
+
+
+# In[4]:
+
 
 print("example engagement scores:")
 first_ex_engagement = util.predict_engagement(model_group_zero, 'Climate change is terrible', TEXT, device).item()
 second_ex_engagement = util.predict_engagement(model_group_one, 'We need to act now to fix climate change', TEXT, device).item()
 print('"Climate change is terrible": ', first_ex_engagement)
 print('"We need to act now to fix climate change": ', second_ex_engagement)
+
+
+# In[ ]:
+
 
 # iterate through words in a unique corpus dictionary
 unique_words = set()
@@ -91,25 +109,32 @@ tweet_to_engagement = defaultdict(list)
 alt_tweet_to_engagement = defaultdict(lambda: defaultdict(list))
 
 tweet_file = pd.read_csv(testFile)
-tweets = tweet_file['text']
-tweets = tweets[:3]
+tweets = tweet_file['clean_text']
+tweets = tweets[:420]
 
+inverse_box = lambda x: (x*(-0.6)+1)**(1/-0.6)
+
+print("predicting engagements")
 for tweet_idx, tweet in enumerate(tweets):
     filtered_words = [word for word in tweet.split(' ') if word not in stopwords.words('english')]
-    # TODO: replace with model here
     group_one_engagement = util.predict_engagement(model_group_one, tweet, TEXT, device).item()
     group_zero_engagement = util.predict_engagement(model_group_zero, tweet, TEXT, device).item()
     for word in filtered_words:
-        tweet_to_engagement[word].append((group_zero_engagement, group_one_engagement))
-
+        tweet_to_engagement[word].append(((group_zero_engagement), (group_one_engagement)))
+print("engagements predictsd")
+        
+print("getting words")
 for tweet_idx, tweet in enumerate(tweets):
     filtered_words = [word for word in tweet.split(' ') if word not in stopwords.words('english')]
     unique_words = unique_words.union(filtered_words)
     for word in filtered_words:
         word_to_tweets[word].append(tweet_idx)
-        
+print("got words")
 
+
+print("getting alts")
 # for each word, get 5 alternatives
+print("len of alts ", len(unique_words))
 for word in unique_words:
     syns = wordnet.synsets(word) 
     alternatives = []
@@ -126,11 +151,12 @@ for word in unique_words:
             tweet = tweets[tweet_idx]
             alt_tweet = tweet.replace(word, alt)
             # recompute engagement score delta across all user groups
-            # TODO: replace with model here
             group_one_engagement = util.predict_engagement(model_group_one, alt_tweet, TEXT, device).item()
             group_zero_engagement = util.predict_engagement(model_group_zero, alt_tweet, TEXT, device).item()
-            alt_tweet_to_engagement[word][alt].append((group_one_engagement, group_zero_engagement))
+            alt_tweet_to_engagement[word][alt].append(((group_one_engagement), (group_zero_engagement)))
+print("got alts")
 # record alt with highest delta
+print("getting replacements")
 replacements = []
 Replacement = namedtuple('Replacement', ['delta', 'original', 'alt'])
 for word in tweet_to_engagement:
@@ -142,11 +168,26 @@ for word in tweet_to_engagement:
         delta = sum(avg_engagement_alt - avg_engagement_orig)
         replacements.append(Replacement(delta, word, alt_word))
 replacements.sort(key=lambda x: x.delta)
+print("got replacements")
 # record top 10 words with highest delta and that is our answer
 print("top 20 words and replacements are")
-for replacement in replacements[-20:]:
-    print("delta: {}, originial: {}, new: {}".format(replacement.delta, replacement.original, replacement.alt))# In[88]:
-# In[89]:
+for replacement in replacements[-20:][::-1]:
+    print("delta: {}, originial: {}, new: {}".format(replacement.delta, replacement.original, replacement.alt))
+
+
+# In[28]:
+
+
+tweet_to_engagement
+
+
+# In[ ]:
+
+
+alt_tweet_to_engagement['dangers']['risk']
+
+
+# In[ ]:
 
 
 
